03cac7f6ed38cb194dcb828fe190c64c606782f2
SiWardCO <141329873+SiWardCO@users.noreply.github.com>
2023-12-08 15:47:13 +0000

    Changed browser behaviour to create a new browser instance every class rather than every test.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,11 @@
 package tests;
 
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
-import pages.userAccess.LoginPage;
 import utilities.Browser;
 import utilities.Users;
-
-import java.time.Duration;
 
 public class BaseTest {
 
@@ -20,13 +14,17 @@ public class BaseTest {
     public Browser browser = new Browser();
 
 
-    @BeforeMethod (alwaysRun = true)
+    @BeforeClass (alwaysRun = true)
     public void setUp() {
         driver = browser.createDriver();
-
     }
 
-    @AfterMethod (alwaysRun = true)
+    @BeforeMethod (alwaysRun = true)
+    public void clearHistory() {
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterClass (alwaysRun = true)
     public void tearDown() {
         browser.closeDriver();
     }
